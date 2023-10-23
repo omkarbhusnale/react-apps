749b01319d7a4397854514affbf9ad67db9fccb8
omkar.bhusnale <omkar.bhusnale@gicindia.com>
2023-10-23 12:17:05 +0530

    Created ReactJs Post Blog

---
+++ src/NewPost.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const NewPost = ({ 
+  handleSubmit, 
+  postTitle, 
+  setPostTitle, 
+  postBody,
+  setPostBody
+}) => {
+  return (
+    <main className='NewPost'>
+      <h2>New Post</h2>
+      <form 
+        className='newPostForm'
+        onSubmit={handleSubmit}
+      >
+        <label htmlFor="postTitle">Title: </label>
+        <input 
+          type="text"  
+          required
+          value={postTitle}
+          onChange={(e) => setPostTitle(e.target.value)}  />
+        <label htmlFor="postBody">Body: </label>
+        <textarea 
+          id="postBody"  
+          required
+          value={postBody}
+          onChange={(e) => setPostBody(e.target.value)}
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </main>
+  )
+}
+
+export default NewPost
